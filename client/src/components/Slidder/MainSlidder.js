d690bd9433440078936c0e5789c5e89664916bbb
skg1998 <sahilkg1998@gmail.com>
2020-11-08 07:44:56 +0530

    improve slidder style

---
+++ client/src/components/Slidder/MainSlidder.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import MobileDetect from "mobile-detect";
+import Image from "../Image/Image";
+import Carousel from "react-multi-carousel";
+import "./style.css";
+import "react-multi-carousel/lib/styles.css";
+
+class Slidder extends Component {
+    static getInitialProps({ req }) {
+        let userAgent;
+        let deviceType;
+        if (req) {
+          userAgent = req.headers["user-agent"];
+        } else {
+          userAgent = navigator.userAgent;
+        }
+        const md = new MobileDetect(userAgent);
+        if (md.tablet()) {
+          deviceType = "tablet";
+        } else if (md.mobile()) {
+          deviceType = "mobile";
+        } else {
+          deviceType = "desktop";
+        }
+        return { deviceType };
+      }
+    
+    state = { isMoving: false };
+
+    render() { 
+        const { classes } = this.props;
+        const images = [
+            "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          ];
+
+        const fakerData = Array(12)
+            .fill(0)
+            .map((item, index) => {
+                return {
+                image: images[index]
+                };
+            });
+            const responsive = {
+            desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 1,
+                slidesToSlide: 1
+            },
+            tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 1,
+                slidesToSlide: 1
+            },
+            mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1,
+                slidesToSlide: 1
+            }
+        };
+        return (
+            <div className={classes.root}>
+                <Carousel
+                    swipeable={true}
+                    draggable={true}
+                    responsive={responsive}
+                    ssr
+                    showDots
+                    infinite
+                    containerClass="container-with-dots"
+                    itemClass="slidder-image"
+                    deviceType={this.props.deviceType}
+                    >
+                    {fakerData.slice(0, 5).map(card => {
+                        return <Image url={card.image} />;
+                    })}
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+const styles = () => ({
+    root: {
+      textAlign: "center"
+    }
+  });
+
+export default withStyles(styles)(Slidder);
