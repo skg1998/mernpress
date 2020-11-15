af4a16eac082b2c56661a15d36255181cbc3b935
skg1998 <sahilkg1998@gmail.com>
2020-11-15 08:51:03 +0530

    improve category slidder

--- client/src/components/Slidder/CategorySlidder.js
+++ client/src/components/Slidder/CategorySlidder.js
@@ -63,17 +63,17 @@ class CategorySlidder extends Component {
     const responsive = {
       desktop: {
         breakpoint: { max: 3000, min: 1024 },
-        items: 3,
+        items: 5,
         slidesToSlide: 3
       },
       tablet: {
         breakpoint: { max: 1024, min: 464 },
-        items: 2,
+        items: 3,
         slidesToSlide: 2
       },
       mobile: {
         breakpoint: { max: 464, min: 0 },
-        items: 1,
+        items: 2,
         slidesToSlide: 1
       }
     };
