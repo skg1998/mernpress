10aa5a7e6c6029b86feab90c81e72f0ac19d5320
skg1998 <sahilkg1998@gmail.com>
2020-11-18 08:33:30 +0530

    change database config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -6,7 +6,7 @@ const config = {
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
-    '/lapimenia',
+    '/mernpress',
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
   stripe_test_api_key: 'YOUR_stripe_test_api_key'
