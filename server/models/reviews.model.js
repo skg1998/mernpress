6883665f5a0cd4288e41e056fc312500b0beb952
skg1998 <sahilkg1998@gmail.com>
2020-12-23 21:37:51 +0530

    update review schema

--- server/models/reviews.model.js
+++ server/models/reviews.model.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
+
 const ReviewSchema = new mongoose.Schema({
     userId:{type:mongoose.Schema.ObjectId, ref:'User'},
     comment: { type: String, required: true },
