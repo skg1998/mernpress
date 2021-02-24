f773c5e154cca5331e894236b5ea271d21039de4
skg1998 <sahilkg1998@gmail.com>
2021-02-24 15:04:51 +0530

    connect store to adminlogin page

--- client/src/admin/Pages/AdminLogin.js
+++ client/src/admin/Pages/AdminLogin.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import { Button,TextField ,Card ,CardHeader, Grid , withStyles } from '@material-ui/core';
-import {Redirect , Link} from 'react-router-dom'
-import SEO from "../../components/SEO/Seo"
-import * as Api from '../../Api';
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { connect } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { adminActions } from '../../store/actions';
+import { Button, TextField, Card, CardHeader, Grid, withStyles } from '@material-ui/core';
+import SEO from "../../components/SEO/Seo";
 
 const useStyles = (theme) => ({
     card: {
@@ -12,88 +14,102 @@ const useStyles = (theme) => ({
         minHeight: "20vh",
         display: "flex",
         alignItems: "center"
-      },
-  });
+    },
+});
 
-class AdminLogin extends Component {
-    state = {
-        email: '',
-        password:'',
-        redirectToReferrer:false
-    };
+function WarningAlert(props) {
+    return <div {...props} />
+}
 
-    handleChange = (event) => {
-        const target = event.target;
-        const field =  target.name;
-        const value = target.value
+const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const classes = useStyles();
 
-        this.setState({
-            [field]:  value
-        });
-    }
+    // reset login status
+    useEffect(() => {
+        dispatch(adminActions.logout());
+    }, []);
 
-    handleSubmit = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const adminLogin = {
-            email: this.state.email || undefined,
-            password:this.state.password || undefined
+        if (email === '' || password === '') {
+            setError("Feilds are Required");
         }
-
-        Api.AdminSignin(adminLogin).then((data) => {
-            if (data.error) {
-              this.setState({error: data.error})
-            } else {
-                this.setState({redirectToReferrer: true})
-            }
-          })
+        props.dispatch(adminActions.login(email, password));
     }
-    render() { 
-        const { classes } = this.props;
-        const {from} = this.props.location.state || {
-            from: {
-                pathname: '/admin'
-            }
-        }
 
-        const {redirectToReferrer} = this.state
-
-        if (redirectToReferrer) {
-            return (<Redirect to={from}/>)
-        }
-
-        return (
-        <div > 
+    return (
+        <div >
             <SEO title="Admin - Login " description="A Multi-vendor ecommerce site" />
             <Grid
-               container
+                container
                 className={classes.card}
                 spacing={0}
                 alignItems="center"
                 justify="center"
                 style={{
-                    position: 'absolute', 
-                    left: '50%', 
+                    position: 'absolute',
+                    left: '50%',
                     top: '50%',
                     transform: 'translate(-50%, -50%)'
                 }}
             >
-                <Card style={{padding:'20px'}}>
-                    <form onSubmit = { this.handleSubmit } noValidate autoComplete="off">
-                        <CardHeader style={{color:'#3f51b5'}} title="Login" />
+                <Card style={{ padding: '20px' }}>
+                    {error && (
+                        <WarningAlert severity="error" onClick={() => setError(null)}>
+                            {props.error || error}
+                        </WarningAlert>
+                    )}
+                    <form onSubmit={handleSubmit} noValidate autoComplete="off">
+                        <CardHeader style={{ color: '#3f51b5' }} title="Login" />
                         <Grid item xs={12}>
-                            <TextField id="outlined-basic" label="Email" variant="outlined"  type = "email" name = "email" onChange= {this.handleChange} style={{marginTop:'10px', width:'100%'}}/>
+                            <TextField
+                                id="outlined-basic"
+                                label="Email"
+                                variant="outlined"
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                style={{ marginTop: '10px', width: '100%' }}
+                            />
                         </Grid>
-                        <Grid item xs={12}>    
-                            <TextField id="outlined-basic" label="Password"  variant="outlined"  type = "password" name = "password" onChange= {this.handleChange} style={{marginTop:'10px', width:'100%'}}/>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Password"
+                                variant="outlined"
+                                type="password"
+                                name="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                style={{ marginTop: '10px', width: '100%' }}
+                            />
                         </Grid>
-                        <Button  variant="contained" color="primary" type = "submit" style={{ marginTop:'10px',width:'100%'}}> SignIn </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            style={{ marginTop: '10px', width: '100%' }}
+                        > SignIn </Button>
                     </form>
                     <Link to="/adminsignup">Sing Up</Link>
                 </Card>
-            </Grid> 
+            </Grid>
         </div>
-        );
-  }
+    );
 }
 
-export default withStyles(useStyles)(AdminLogin);
+function mapStateToProps(state) {
+    const { loggingIn } = state.authentication;
+    return {
+        loggingIn
+    };
+}
+
+const connectedLoginPage = connect(mapStateToProps)(Login);
+export { connectedLoginPage as AdminLogin }; 
