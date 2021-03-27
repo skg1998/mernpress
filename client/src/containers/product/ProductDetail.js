10a146198915502f4f9eb10c1af28e61a9f8aa78
skg1998 <sahilkg1998@gmail.com>
2021-03-27 20:42:43 +0530

    create : create product detail component

--- client/src/containers/product/ProductDetail.js
+++ client/src/containers/product/ProductDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ImageGallery from '../../components/Image/imageGallery'
 
 
 const ProductDetail = (props) => {
     const { data } = props;
     return (
         <div>
-
+            <ImageGallery />
         </div>
     )
 }
