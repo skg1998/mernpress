42941c74a87e63a631de97ec4ccdd92d6e1f2fb1
skg1998 <sahilkg1998@gmail.com>
2021-05-10 12:59:33 +0530

    fix(data): modifie product data

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -12,7 +12,138 @@ import Products from '../containers/product/Products'
 import SEO from '../components/SEO/Seo';
 
 const data = [
-  { _id: "1" }
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  },
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  },
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  },
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  },
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  },
+  {
+    _id: "1",
+    name: "Redmi Note 9 Pro Max",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71usqWEJleL._SL1500_.jpg",
+    description: "Country Of Origin - India 64MP quad rear camera with ultra-wide, macro mode, super macro, portrait, night mode, 960fps slowmotion, ai scene recognition, pro color, HDR, pro mode | 32MP front camera 16.94 centimeters (6.67 inch) FHD+ LCD multi-touch capacitive touchscreen, full screen dot display with 2400 x 1080 pixels resolution, 400 ppi pixel density and 20:9 aspect ratio | 2.5D curved glass",
+    sku: "KS935FGH",
+    metaTagTitle: "(Champagne Gold, 6GB RAM, 64GB Storage) - 64MP Quad Camera & Latest 8nm Snapdragon 720G & Alexa Hands-Free",
+    upc: "",
+    modal: "Redmi",
+    quantity: 67,
+    price: 14999.00,
+    outOfStockStatus: false,
+    requiredShipping: true,
+    dateAvailable: "",
+    condition: [],
+    status: 2,
+    sortOrder: 45,
+    category: [],
+    discount: [],
+    shop: [],
+    review: { rating: 4, totalreview: 445 }
+  }
 ]
 
 const styles = theme => ({
@@ -41,7 +172,7 @@ class Home extends Component {
           {/* Main-banner */}
           <BannerContainer />
           {/* (Product) */}
-          <Products products={data} />
+          <Products products={data} name={"New Deals"} />
         </div>
         <Footer />
       </div>
