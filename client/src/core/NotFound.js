6955ef3dad8c6b6ec5cf9e7b8572ff00879c7b81
skg1998 <sahilkg1998@gmail.com>
2021-05-10 14:25:57 +0530

    feat(404page): create 404-notfound page

---
+++ client/src/core/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Notfound from '../assets/img/error-404.svg';
+
+const NotFound = () => (
+    <div>
+        <img src={Notfound} style={{ width: '100%', height: "100%" }} />
+    </div>
+);
+
+export default NotFound;
