8dc88cd82d7bee5f27b4cc6004e6f01420c91d55
Sahil Gupta <49576577+skg1998@users.noreply.github.com>
2021-05-13 10:32:48 +0530

    Revert "added login and signup"

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 
-// import Header from '../components/Navbars/Header'
+import Header from '../components/Navbars/Header'
 import MainSlidder from '../components/Slidder/MainSlidder'
 import CategoriesBar from '../components/Slidder/CategoriesBar'
 import BannerContainer from '../containers/Banner/BannerContainer'
 import Footer from '../components/Footer/Footer'
 import Products from '../containers/product/Products'
-import Header from '../components/header/header.js';
 
 import SEO from '../components/SEO/Seo';
 
@@ -165,7 +164,7 @@ class Home extends Component {
     return (
       <div>
         <SEO title="MernPress - Ecommerce site " description="A Multi-vendor ecommerce site" />
-        <Header/>
+        <Header />
         <div className={classes.root}>
           {/* <CategoriesBar className={classes.categories} /> */}
           {/* Main-slidder */}
