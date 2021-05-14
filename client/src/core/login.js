3c906e2ce5b6d26134917e79d7c0c138b6eccb9a
skg1998 <sahilkg1998@gmail.com>
2021-05-14 12:21:25 +0530

    style(UI): style login component

--- client/src/core/login.js
+++ client/src/core/login.js
@@ -9,33 +9,40 @@ import Typography from '@material-ui/core/Typography'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import Dialog from '@material-ui/core/Dialog'
+import Grid from '@material-ui/core/Grid'
 import { DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core"
 
 import SEO from '../components/SEO/Seo';
 
 const useStyles = makeStyles((theme) => ({
     card: {
-        maxWidth: 600,
-        margin: 'auto',
-        textAlign: 'center',
-        marginTop: theme.spacing.unit * 5,
-        paddingBottom: theme.spacing.unit * 2
+        padding: '10px',
+        boxShadow: '0 4px 9px 0 rgba(0,0,0,0.2)'
     },
     error: {
         verticalAlign: 'middle'
     },
     title: {
-        marginTop: theme.spacing.unit * 2,
-        color: theme.palette.openTitle
+        color: '#4C3A98',
+        display: 'flex',
+        justifyContent: 'center',
+        height: '20%',
+        fontSize: '3rem',
+        fontWeight: 'bold',
     },
     textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 300
+        marginTop: '10px',
+        width: '100%'
     },
     submit: {
-        margin: 'auto',
-        marginBottom: theme.spacing.unit * 2
+        backgroundColor: '#ff0c0c',
+        padding: '10px',
+        width: '100%',
+        textAlign: 'center',
+        color: '#FFFFFF',
+        border: 'none',
+        borderRadius: ' 15px',
+        boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)'
     }
 }))
 
@@ -64,62 +71,75 @@ const Signin = () => {
     return (
         <div>
             <SEO title="SignIn - Mernpress " description="A Multi-vendor ecommerce site" />
-            <form onSubmit={handleSubmit}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Typography
-                            type="headline"
-                            component="h2"
-                            className={classes.title}
-                        >
-                            Sign In
+            <Grid
+                container
+                spacing={0}
+                alignItems="center"
+                justify="center"
+                style={{
+                    position: 'absolute',
+                    left: '50%',
+                    top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}
+            >
+                <form onSubmit={handleSubmit}>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography
+                                type="headline"
+                                component="h2"
+                                className={classes.title}
+                            >
+                                Sign In
                         </Typography>
-                        <TextField
-                            id="email"
-                            type="email"
-                            label="Email"
-                            className={classes.textField}
-                            value={input.email}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            id="password"
-                            type="password"
-                            label="Password"
-                            className={classes.textField}
-                            value={input.password}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            color="primary"
-                            raised="raised"
-                            type="submit"
-                            className={classes.submit}
-                        >
-                            Submit
-                        </Button>
-                    </CardActions>
-                </Card>
-            </form>
-            <Dialog open={submitted} disableBackdropClick={true}>
-                <DialogTitle>New Account</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Login successfully .
+                            <TextField
+                                id="email"
+                                type="email"
+                                label="Email"
+                                variant="outlined"
+                                className={classes.textField}
+                                value={input.email}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                id="password"
+                                type="password"
+                                label="Password"
+                                variant="outlined"
+                                className={classes.textField}
+                                value={input.password}
+                                onChange={handleChange}
+                            />
+                        </CardContent>
+                        <CardActions>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Submit
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </form>
+                <Dialog open={submitted} disableBackdropClick={true}>
+                    <DialogTitle>New Account</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Login successfully .
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Link to="/signup">
-                        <Button color="primary" autoFocus="autoFocus" variant="raised">
-                            Sign Up
+                    </DialogContent>
+                    <DialogActions>
+                        <Link to="/signup">
+                            <Button color="primary" autoFocus="autoFocus" variant="raised">
+                                Sign Up
                         </Button>
-                    </Link>
-                </DialogActions>
-            </Dialog>
+                        </Link>
+                    </DialogActions>
+                </Dialog>
+            </Grid>
         </div>
     );
 }
