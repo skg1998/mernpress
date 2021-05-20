d2bd5600aaef81f62795ec01c64846435c8eca42
skg1998 <sahilkg1998@gmail.com>
2021-05-20 17:52:36 +0530

    feat(middleware): create a middleware to handle error

---
+++ server/src/middleware/hasAuth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.model');
+const ErrorResponse = require('../util/errorResponse');
+
+exports.hasAuthorization = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(" ")[1]
+    }
+
+    //Make sure token exist
+    if (!token) {
+        return next(new ErrorResponse('Not Authorized to access this route', 401))
+    }
+
+    try {
+        //verify token
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decode)
+        req.user = await User.find(decode.id)
+    } catch (err) {
+        return next(new ErrorResponse('Not Authorized to access this route', 401))
+    }
+}
+
