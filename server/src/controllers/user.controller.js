e4f2709234107c98f665ca183662cddb77ae0eb6
skg1998 <sahilkg1998@gmail.com>
2021-05-20 18:03:49 +0530

    feat(userController update): create user controller create, login

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,68 +1,86 @@
 const User = require("../models/user.model");
-const errorHandler = require("../util/dbErrorHandler");
-const config = require('../config/config');
-const stripe = require("stripe");
-const myStripe = stripe(config.stripe_test_secret_key);
+const ErrorResponse = require('../util/errorResponse');
 
+//get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res, message) => {
+  //Create Token
+  const token = user.getSignedJwtToken();
+  const option = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  }
 
-const create = async (req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    option.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, option)
+    .json({
+      success: true,
+      token: token,
+      message: message
+    })
+}
+
+/**
+ * 
+ * @desc create User account
+ * @route POST api/v1/users/register
+ * @param Public
+ */
+exports.create = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
-    const data = {
-      username: username,
-      email: email,
-      password: password
-    }
-
-    const checkEmail = await User.findOne({ email: email });
-    if (checkEmail) {
-      return res.status(400).json({
-        status: "fail",
-        message: "email is already in use , Please try with other email"
-      })
-    }
-
-    const newUser = await User.create(data);
-
-    res.status(200).json({
-      status: "succes",
-      data: newUser,
-      message: "succesfull signup !",
-    })
-
+    const user = await User.create({ username, email, password });
+    sendTokenResponse(user, 200, res, "account has been created successfully !");
   } catch (err) {
-    res.status(404).json({
-      status: false,
-      message: " bad request"
-    })
-  }
-};
-
-const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const data = {
-      email: email,
-      password: password
-    }
-    const user = await User.findOne(req.body.email);
-    if (!user) {
-      return res.status("401").json({
-        status: false,
-        message: `User Not found by given email: ${req.body.email}`
-      });
-    }
-
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      message: "Something went wrong, try after some time"
-    })
+    next(err);
   }
 }
 
+/**
+ * 
+ * @desc login to account
+ * @route POST api/v1/users/login
+ * @param Public
+ */
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-const list = async (req, res, next) => {
+    //Validate email & password
+    if (!email && !password) {
+      return next(new ErrorResponse('Please Provide an email and password', 400))
+    }
+
+    //CHECK USER
+    const user = await User.find({ email: email }).select('+password')
+    if (!user) {
+      return next(new ErrorResponse('Invalid credentials', 401))
+    }
+
+    //Check hashed Password
+    const isMatched = await user.matchedPassword(password);
+    if (!isMatched) {
+      return next(new ErrorResponse('Invalid credentials', 401))
+    }
+
+    sendTokenResponse(user, 200, res, "login  successfully !");
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ * 
+ * @desc list of all users
+ * @route GET api/v1/users/list
+ * @param Public
+ */
+exports.list = async (req, res, next) => {
   try {
     const users = await User.find();
     res.status(201).json({
@@ -72,14 +90,17 @@ const list = async (req, res, next) => {
       message: `All Users found succesfully`
     })
   } catch (err) {
-    res.status(400).json({
-      status: false,
-      message: `Something went wrong`
-    })
+    next(err);
   }
 };
 
-const userByID = (req, res, next) => {
+/**
+ * 
+ * @desc list of user by id
+ * @route POST api/v1/users/userbyid
+ * @param Public
+ */
+exports.userByID = async (req, res, next) => {
   try {
     const user = await User.find(req.params.id);
 
@@ -97,14 +118,17 @@ const userByID = (req, res, next) => {
     })
 
   } catch (err) {
-    res.status(400).json({
-      status: false,
-      message: `Something went wrong`
-    })
+    next(err)
   }
-};
+}
 
-const update = async (req, res, next) => {
+/**
+ * 
+ * @desc Update account
+ * @route PUT api/v1/users/:id
+ * @param Private
+ */
+exports.update = async (req, res, next) => {
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -126,14 +150,17 @@ const update = async (req, res, next) => {
     })
 
   } catch (err) {
-    res.status(400).json({
-      status: false,
-      message: `User not fount of given id: ${req.params.id}`
-    })
+    next(err);
   }
-};
+}
 
-const remove = async (req, res, next) => {
+/**
+ * 
+ * @desc delete account
+ * @route DELETE api/v1/users/:id
+ * @param Private
+ */
+exports.remove = async (req, res, next) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id)
 
@@ -151,105 +178,6 @@ const remove = async (req, res, next) => {
     })
 
   } catch (err) {
-    res.status(400).json({
-      status: false,
-      message: `User not fount of given id: ${req.params.id}`
-    })
-  }
-};
-
-const stripe_auth = (req, res, next) => {
-  request(
-    {
-      url: "https://connect.stripe.com/oauth/token",
-      method: "POST",
-      json: true,
-      body: {
-        client_secret: config.stripe_test_secret_key,
-        code: req.body.stripe,
-        grant_type: "authorization_code"
-      }
-    },
-    (error, response, body) => {
-      if (body.error) {
-        return res.status("400").json({
-          error: body.error_description
-        });
-      }
-      req.body.stripe_seller = body;
-      next();
-    }
-  );
-};
-
-
-const stripeCustomer = (req, res, next) => {
-  if (req.profile.stripe_customer) {
-    //update stripe customer
-    myStripe.customers.update(req.profile.stripe_customer, {
-      source: req.body.token
-    }, (err, customer) => {
-      if (err) {
-        return res.status(400).send({
-          error: "Could not update charge details"
-        })
-      }
-      req.body.order.payment_id = customer.id
-      next()
-    })
-  } else {
-    myStripe.customers.create({
-      email: req.profile.email,
-      source: req.body.token
-    }).then((customer) => {
-      User.update({ '_id': req.profile._id },
-        { '$set': { 'stripe_customer': customer.id } },
-        (err, order) => {
-          if (err) {
-            return res.status(400).send({
-              error: errorHandler.getErrorMessage(err)
-            })
-          }
-          req.body.order.payment_id = customer.id
-          next()
-        })
-    })
+    next(err)
   }
 }
-
-
-const createCharge = (req, res, next) => {
-  if (!req.profile.stripe_seller) {
-    return res.status('400').json({
-      error: "Please connect your Stripe account"
-    })
-  }
-  myStripe.tokens.create({
-    customer: req.order.payment_id,
-  }, {
-    stripe_account: req.profile.stripe_seller.stripe_user_id,
-  }).then((token) => {
-    myStripe.charges.create({
-      amount: req.body.amount * 100, //amount in cents
-      currency: "usd",
-      source: token.id,
-    }, {
-      stripe_account: req.profile.stripe_seller.stripe_user_id,
-    }).then((charge) => {
-      next()
-    })
-  })
-}
-
-module.exports = {
-  isSeller,
-  create,
-  login,
-  userByID,
-  list,
-  remove,
-  update,
-  stripe_auth,
-  stripeCustomer,
-  createCharge
-};
