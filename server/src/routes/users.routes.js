655ead57bfa95f08a0061a9d2d71fba390c5d71e
skg1998 <sahilkg1998@gmail.com>
2021-05-24 12:10:52 +0530

    fix(protect): protect user routes

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,20 +1,30 @@
 const express = require("express");
-const { create, login, update, remove, list, userByID } = require("../controllers/user.controller");
+const {
+    create,
+    login,
+    update,
+    remove,
+    list,
+    userByID,
+    getMyProfile,
+    logout,
+    forgotPassword,
+    resetPassword
+} = require("../controllers/user.controller");
+
 const { hasAuthorization } = require('../middleware/hasAuth')
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(list)
-
-router.post('/signup', create);
-router.post('/signin', login);
-
-router
-  .route("/:id")
-  .get(userByID)
-  .put(hasAuthorization, update)
-  .delete(hasAuthorization, remove);
+router.route("/").get(list)
+router.route("/:id").get(userByID)
+router.route('/signup').post(create);
+router.route('/signin').post(login);
+router.route('/logout').get(logout);
+router.route('/myProfile').get(hasAuthorization, getMyProfile);
+router.route('/updateDetail').put(hasAuthorization, update);
+router.route('/:id').put(hasAuthorization, remove);
+router.route('/forgotPassword').post(forgotPassword);
+router.route('/resetPassword/:resetToken').put(resetPassword);
 
 module.exports = router;
