7f59b948e6a16bb2434d1a29acce3c7da39b1401
skg1998 <sahilkg1998@gmail.com>
2021-05-26 10:13:06 +0530

    feat(middleware): update multer middleware

--- server/src/routes/Banner.routes.js
+++ server/src/routes/Banner.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Upload = require('../util/uploadFile');
+const Upload = require('../middleware/uploadFile');
 const BannerCtrl = require("../controllers/Banner.controller");
 
 const router = express.Router();
