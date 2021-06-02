42ed04cbba84edb74082aeab332deab9c8d6324f
skg1998 <sahilkg1998@gmail.com>
2021-06-02 17:32:28 +0530

    feat(authrize_middleware): add middleware to check role

--- server/src/routes/Slidder.routes.js
+++ server/src/routes/Slidder.routes.js
@@ -1,22 +1,20 @@
 const express = require("express");
+const { hasAuthorization, authorize } = require('../middleware/hasAuth');
 const slidderCtrl = require("../controllers/Slidder.controller");
 
 const router = express.Router();
 
 router
   .route("/")
-  .get(slidderCtrl.read); 
+  .get(slidderCtrl.read);
 
 router
   .route("/")
-  .post(slidderCtrl.create);
-  
-router
-  .route("/")
-  .put(slidderCtrl.update); 
-  
-router
-  .route("/")
-  .delete(slidderCtrl.remove);  
+  .post(hasAuthorization, authorize('admin'), slidderCtrl.create);
 
-module.exports = router;  
+router
+  .route("/:id")
+  .put(hasAuthorization, authorize('admin'), slidderCtrl.update)
+  .delete(hasAuthorization, authorize('admin'), slidderCtrl.remove);
+
+module.exports = router;
