4e4855e3dd8f89ae716cb63339a566ed135703ae
skg1998 <sahilkg1998@gmail.com>
2021-07-09 11:39:39 +0530

    feat(blog): create blog schema

---
+++ server/src/models/blog.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const BlogCategorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const BlogCategory = mongoose.model("BlogCategory", BlogCategorySchema);
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: false,
+    },
+    username: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    categories: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'BlogCategory'
+    },
+},
+    { timestamps: true }
+);
+
+const Blog = mongoose.model("Blog", BlogSchema);
+
+module.exports = { BlogCategory, Blog }
