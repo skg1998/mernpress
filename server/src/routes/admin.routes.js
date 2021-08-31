13e5a095d50a0ebdaa05b1e6c19e15b38140c368
skg1998 <sahilkg1998@gmail.com>
2021-08-31 21:07:40 +0530

    feat(adminRoutes): create admin routes

---
+++ server/src/routes/admin.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const {
+    create,
+    login,
+    update,
+    remove,
+    list,
+    userByID,
+    getMyProfile,
+    logout,
+    forgotPassword,
+    resetPassword
+} = require("../controllers/admin.controller");
+
+const { hasAuthorization } = require('../middleware/hasAuth')
+
+const router = express.Router();
+
+router.route("/").get(list)
+router.route("/:id").get(userByID)
+router.route('/signup').post(create);
+router.route('/signin').post(login);
+router.route('/logout').get(logout);
+router.route('/myProfile').get(hasAuthorization, getMyProfile);
+router.route('/updateDetail').put(hasAuthorization, update);
+router.route('/:id').put(hasAuthorization, remove);
+router.route('/forgotPassword').post(forgotPassword);
+router.route('/resetPassword/:resetToken').put(resetPassword);
+
+module.exports = router;
